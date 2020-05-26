748161edb95ac5bd1aa00d81095ebab738eb6723
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-05-25 20:14:09 -0400

    Loadable from external link. Added info component

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -4,11 +4,29 @@ import {
   BrowserRouter as Router,
   Redirect,
   Route,
-  Switch
+  Switch,
+  Link
 } from 'react-router-dom';
 import loader from '../loader.svg';
 
+import Info from './Info';
+
 class Restaurant extends Component {
+  componentDidMount () {
+    if (!this.props.chosenRestaurant) {
+      this.props.setRestaurant(this.props.match.params.id);
+    }
+    this.props.getInfo(this.props.match.params.id);
+    this.props.getReviews(this.props.match.params.id);
+  }
+
+  componentDidUpdate (prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.props.getInfo(this.props.match.params.id);
+      this.props.getReviews(this.props.match.params.id);
+    }
+  }
+
   render () {
     const info = this.props.info;
     const reviews = this.props.reviews;
@@ -22,8 +40,12 @@ class Restaurant extends Component {
             userLocation={this.props.userLocation}
           /> */}
 
-          {reviews && info
-            ? <div>Details</div>
+          {(reviews && info)
+            ? <Info
+              info={info}
+              reviews={reviews}
+              match={this.props.match}
+            />
 
             : <img src={loader} alt='loading' />}
 
