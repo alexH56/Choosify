9825b8f31bf8f6904a2710144b28720e8719e978
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-05-30 05:59:49 -0400

    Uploading build folder for deployment

---
+++ build/precache-manifest.612367a0f6d6dc77551995a7ddf06fb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfe57641b8c51c0a44ca303aeac95e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ebcf89f47d371acc637",
+    "url": "/static/css/main.70d03190.chunk.css"
+  },
+  {
+    "revision": "c2f08f673fec733ce0cb",
+    "url": "/static/js/2.59ef5a41.chunk.js"
+  },
+  {
+    "revision": "fb335b2a82fc0252a5b6d1840e51a26f",
+    "url": "/static/js/2.59ef5a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebcf89f47d371acc637",
+    "url": "/static/js/main.5e649a06.chunk.js"
+  },
+  {
+    "revision": "9d3c020ec8f18107dfea",
+    "url": "/static/js/runtime-main.f46143e1.js"
+  },
+  {
+    "revision": "9db620335f97c2b2f78da5e43fcd2656",
+    "url": "/static/media/Yelp_Logo.9db62033.svg"
+  },
+  {
+    "revision": "f899c03a3dd387562d9da65beb8cea98",
+    "url": "/static/media/loader.f899c03a.svg"
+  }
+]);
