2d92f89d881f3780da72e1761e6e64132a2663da
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-05-30 05:21:29 -0400

    Added greeting text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import '../App.css';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-  Link
-} from 'react-router-dom';
-
-import LocationForm from './LocationForm.js';
+import '../App.scss';
 
 class Home extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-
-          <p>Home</p>
-
-        </header>
-
-      </div>
+      <main>
+        <div className='homeContainer'>
+          <p>Do you struggle choosing a place to eat?</p>
+          <p>Do you and your significant other go around in circles when picking a restaurant, all the while insisting "anywhere is fine...except there"?</p>
+          <p>Well worry no more! Simply enter your location above and we'll choose for you. Trust in the app.</p>
+        </div>
+      </main>
     );
   }
 }
